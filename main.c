b8c38471077bd437daef49f52b8980c1ad8cf6ed
Alina <haslavskayaalina@icloud.com>
2025-10-31 18:51:02 +0300

    main.c

--- main.c
+++ main.c
@@ -14,16 +14,7 @@ void free_matrix(float **M, int rows) {
     free(M[i]);
   free(M);
 }
-void cleanup(FILE *in, FILE *out, float **A, int AR, float **B, int BR) {
-  free_matrix(A, AR);
-  free_matrix(B, BR);
-  if (in)
-    fclose(in);
-  if (out)
-    fclose(out);
-}
 
-<<<<<<< HEAD
 void cleanup(FILE *in, FILE *out, float **A, int AR, float **B, int BR) {
   if (A)
     free_matrix(A, AR);
@@ -45,7 +36,6 @@ float opr(float **mat, int n) {
 
   float d = 0;
   for (int j = 0; j < n; j++) {
-    /* allocate minor with safety (initialize to NULL) */
     float **minor = malloc((n - 1) * sizeof(float *));
     if (!minor)
       return 0;
@@ -84,44 +74,6 @@ float opr(float **mat, int n) {
     free_matrix(minor, n - 1);
   }
   return d;
-=======
-float opr(float **mat, int n) {
-  if (n == 0)
-    return 0;
-  if (n == 1)
-    return mat[0][0];
-  if (n == 2)
-    return mat[0][0] * mat[1][1] - mat[0][1] * mat[1][0];
-
-  float d = 0;
-  for (int j = 0; j < n; j++) {
-    float **minor = malloc((n - 1) * sizeof(float *));
-    if (!minor)
-      return 0;
-    for (int i = 0; i < n - 1; i++) {
-      minor[i] = malloc((n - 1) * sizeof(float));
-      if (!minor[i])
-        return 0;
-    }
-
-    for (int r = 1; r < n; r++) {
-      int cIndex = 0;
-      for (int c = 0; c < n; c++) {
-        if (c == j)
-          continue;
-        minor[r - 1][cIndex++] = mat[r][c];
-      }
-    }
-
-    float minorOpr = opr(minor, n - 1);
-    if (j % 2 == 0)
-      d += mat[0][j] * minorOpr;
-    else
-      d -= mat[0][j] * minorOpr;
-
-    free_matrix(minor, n - 1);
-  }
-  return d;
 }
 
 void print_matrix(FILE *out, float **M, int n, int m) {
@@ -134,7 +86,6 @@ void print_matrix(FILE *out, float **M, int n, int m) {
     }
     fprintf(out, "\n");
   }
->>>>>>> 1f168f2 (main.c)
 }
 
 int main(int argc, char *argv[]) {
@@ -144,20 +95,13 @@ int main(int argc, char *argv[]) {
   FILE *out = NULL;
   float **A = NULL, **B = NULL;
   int AR = 0, AC = 0, BR = 0, BC = 0;
-
-<<<<<<< HEAD
-  FILE *in = NULL;
-  FILE *out = NULL;
-  float **A = NULL, **B = NULL;
-  int AR = 0, AC = 0, BR = 0, BC = 0;
   int P = 0;
-=======
+
   if (argc != 3) {
     fprintf(stderr, "Incorrect data, an error has occurred.\n");
     result = ERROR_DATA_INVALID;
     goto lCleanup;
   }
->>>>>>> 1f168f2 (main.c)
 
   in = fopen(argv[1], "r");
   if (!in) {
@@ -201,30 +145,6 @@ int main(int argc, char *argv[]) {
     goto lCleanup;
   }
 
-  A = malloc(AR * sizeof(float *));
-  if (!A) {
-    result = ERROR_OUT_OF_MEMORY;
-    goto lCleanup;
-  }
-  for (int i = 0; i < AR; i++) {
-    A[i] = malloc(AC * sizeof(float));
-    if (!A[i]) {
-      result = ERROR_OUT_OF_MEMORY;
-      goto lCleanup;
-    }
-    for (int j = 0; j < AC; j++) {
-      if (fscanf(in, "%f", &A[i][j]) != 1) {
-        result = ERROR_DATA_INVALID;
-        goto lCleanup;
-      }
-    }
-  }
-
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-  /* allocate A safely and initialize pointers to NULL */
->>>>>>> 9e5ee12 (resolve merge conflict in main.c)
   A = malloc(AR * sizeof(float *));
   if (!A) {
     result = ERROR_OUT_OF_MEMORY;
@@ -403,133 +323,6 @@ int main(int argc, char *argv[]) {
     float det = opr(A, AR);
     fprintf(out, "%g\n", det);
   }
-=======
-  int P = 0;
-
-  if (operand == '+' || operand == '-' || operand == '*') {
-    if (fscanf(in, "%d %d", &BR, &BC) != 2) {
-      result = ERROR_DATA_INVALID;
-      goto lCleanup;
-    }
-
-    B = malloc(BR * sizeof(float *));
-    if (!B) {
-      result = ERROR_OUT_OF_MEMORY;
-      goto lCleanup;
-    }
-    for (int i = 0; i < BR; i++) {
-      B[i] = malloc(BC * sizeof(float));
-      if (!B[i]) {
-        result = ERROR_OUT_OF_MEMORY;
-        goto lCleanup;
-      }
-      for (int j = 0; j < BC; j++) {
-        if (fscanf(in, "%f", &B[i][j]) != 1) {
-          result = ERROR_DATA_INVALID;
-          goto lCleanup;
-        }
-      }
-    }
-  } else if (operand == '^') {
-    if (fscanf(in, "%d", &P) != 1) {
-      result = ERROR_DATA_INVALID;
-      goto lCleanup;
-    }
-  }
-
-  if (operand == '+') {
-    if (AR != BR || AC != BC) {
-      fprintf(out, "no solution\n");
-      result = ERROR_DATA_INVALID;
-      goto lCleanup;
-    }
-    for (int i = 0; i < AR; i++)
-      for (int j = 0; j < AC; j++)
-        A[i][j] += B[i][j];
-    print_matrix(out, A, AR, AC);
-  } else if (operand == '-') {
-    if (AR != BR || AC != BC) {
-      fprintf(out, "no solution\n");
-      result = ERROR_DATA_INVALID;
-      goto lCleanup;
-    }
-    for (int i = 0; i < AR; i++)
-      for (int j = 0; j < AC; j++)
-        A[i][j] -= B[i][j];
-    print_matrix(out, A, AR, AC);
-  } else if (operand == '*') {
-    if (AC != BR) {
-      fprintf(out, "no solution\n");
-      result = ERROR_DATA_INVALID;
-      goto lCleanup;
-    }
-    float **C = malloc(AR * sizeof(float *));
-    if (!C) {
-      result = ERROR_OUT_OF_MEMORY;
-      goto lCleanup;
-    }
-    for (int i = 0; i < AR; i++) {
-      C[i] = malloc(BC * sizeof(float));
-      if (!C[i]) {
-        result = ERROR_OUT_OF_MEMORY;
-        goto lCleanup;
-      }
-      for (int j = 0; j < BC; j++) {
-        C[i][j] = 0;
-        for (int k = 0; k < AC; k++)
-          C[i][j] += A[i][k] * B[k][j];
-      }
-    }
-    print_matrix(out, C, AR, BC);
-    free_matrix(C, AR);
-  } else if (operand == '^') {
-    if (AR != AC) {
-      fprintf(out, "no solution\n");
-      result = ERROR_DATA_INVALID;
-      goto lCleanup;
-    }
-    int n = AR;
-    float **res = malloc(n * sizeof(float *));
-    float **tmp = malloc(n * sizeof(float *));
-    if (!res || !tmp) {
-      result = ERROR_OUT_OF_MEMORY;
-      goto lCleanup;
-    }
-    for (int i = 0; i < n; i++) {
-      res[i] = malloc(n * sizeof(float));
-      tmp[i] = malloc(n * sizeof(float));
-      if (!res[i] || !tmp[i]) {
-        result = ERROR_OUT_OF_MEMORY;
-        goto lCleanup;
-      }
-      for (int j = 0; j < n; j++)
-        res[i][j] = A[i][j];
-    }
-
-    for (int p = 1; p < P; p++) {
-      for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++) {
-          tmp[i][j] = 0;
-          for (int k = 0; k < n; k++)
-            tmp[i][j] += res[i][k] * A[k][j];
-        }
-      for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-          res[i][j] = tmp[i][j];
-    }
-    print_matrix(out, res, n, n);
-    free_matrix(res, n);
-    free_matrix(tmp, n);
-  } else if (operand == '|') {
-    if (AR != AC) {
-      fprintf(out, "no solution\n");
-      result = ERROR_DATA_INVALID;
-      goto lCleanup;
-    }
-    float det = opr(A, AR);
-    fprintf(out, "%g\n", det);
-  }
->>>>>>> 1f168f2 (main.c)
 
 lCleanup:
   cleanup(in, out, A, AR, B, BR);
